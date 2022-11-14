75f6cd85422b5f8cae042adf473cbf48f8bc4b16
CECK <az.ah.qwerty@gmail.com>
2022-11-14 21:05:59 +0500

    scoreSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,9 @@
-import { combineReducers } from "redux";
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  reducer: {},
+})
 
 
-
-
-export const store = combineReducers({
-
-}, )
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
